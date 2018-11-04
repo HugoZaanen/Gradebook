a197431974bfa541b0a5917bb55b35d9891aeece
Huego <hugozaanen@hotmail.com>
2018-11-04 18:14:10 +0100

    Gradebook

--- Gradebook/Program.cs
+++ Gradebook/Program.cs
@@ -10,6 +10,12 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            //one-dimensional array of student grades
+            int[] gradesArray = {87,68,94,100,83,78,85,91,76,87};
+
+            var myGradebook = new GradeBook(
+                "CS101 Introduction to C# Programming", gradesArray);
+            myGradebook.ProcessGrades();
         }
     }
 }
